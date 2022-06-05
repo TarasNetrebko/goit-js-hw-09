a38835c4fe8e1f3b08384449cb71d254642a007d
Taras <93833612+TarasNetrebko@users.noreply.github.com>
2022-06-05 19:51:22 +0300

    v1.1.0

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -39,8 +39,14 @@ function startTimerHandler() {
 
 function timer() {
     // console.log("Tik");
-    const value = convertMs(fp.selectedDates[0] - new Date());
-    addLeadingZero(value);    
+    const timeLeft = fp.selectedDates[0] - new Date();
+    const value = convertMs(delta);
+    if (timeLeft <= 0) {
+        clearInterval(interval);
+        Notiflix.Notify.success("Yep! Time's up");
+        return;
+    }
+    addLeadingZero(value);
 }    
 
 function convertMs(ms) {
@@ -64,16 +70,6 @@ function addLeadingZero(dateObj) {
     renderHours.textContent = dateObj.hours.toString().length === 2 ? dateObj.hours : `${dateObj.hours}`.padStart(2, "0");
     renderMinutes.textContent = dateObj.minutes.toString().length === 2 ? dateObj.minutes : `${dateObj.minutes}`.padStart(2, "0");
     renderSeconds.textContent = dateObj.seconds.toString().length === 2 ? dateObj.seconds : `${dateObj.seconds}`.padStart(2, "0");
-    if (dateObj.days <= 0 && dateObj.hours <= 0 && dateObj.minutes <= 0 && dateObj.seconds <= 0) {
-        Notiflix.Notify.success("Yep! Time's up");
-        clearInterval(interval);
-        // тут не обов'язково таке писати мабуть, але в календарі можна вибрати минулу дату і клацнути ентер
-        // в результаті алерт виб'є і кнопка буде disabled, але все одно відрендерить від'ємні числа
-        renderDays.textContent = "00";
-        renderHours.textContent = "00";
-        renderMinutes.textContent = "00";
-        renderSeconds.textContent = "00";
-    }
 }
 
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from "notiflix";
+
+const form = document.querySelector(".form");
+const submitBtn = document.querySelector("button");
+const delay = document.querySelector(".delay");
+const amount = document.querySelector(".amount");
+const step = document.querySelector(".step");
+submitBtn.addEventListener("click", promisesHandler);
+  
+function promisesHandler(e) {
+  e.preventDefault();
+  let resultDelay = Number(delay.value);
+  for (let i = 1; i <= amount.value; i += 1) {
+      createPromise(i, resultDelay);
+      resultDelay += Number(step.value);
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  setTimeout(() => {
+    if (shouldResolve) {
+    // Fulfill    
+      Promise.resolve(`Fulfilled promise ${position} in ${delay}ms`).then((value) => Notiflix.Notify.success(value));  
+  } else {
+    // Reject
+      Promise.reject(`Rejected promise ${position} in ${delay}ms`).catch((error) => Notiflix.Notify.failure(error));
+  }
+  }, delay)
+  
+}
+
+
+// Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`)
+// Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`)
